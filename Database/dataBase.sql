39b4b24686ad12b80c88aa89c2bc5d0c66106c28
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-01-03 20:50:25 +0100

    Table Articles

--- Database/dataBase.sql
+++ Database/dataBase.sql
@@ -10,3 +10,14 @@ CREATE TABLE Users (
     role ENUM('user', 'admin') DEFAULT 'user'
 );
 
+-- Table Articles
+CREATE TABLE Articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    image VARCHAR(255),
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    userId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE
+);
