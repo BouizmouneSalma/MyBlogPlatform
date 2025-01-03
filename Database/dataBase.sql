b8b9a09e7822b345df928fd560b3cec1c522c4ac
BouizmouneSalma <salmabouizmoun@gmail.com>
2025-01-03 20:49:47 +0100

    Table User

--- Database/dataBase.sql
+++ Database/dataBase.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE ProjetGestionArticles;
 USE ProjetGestionArticles;
+
+-- Table Users
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') DEFAULT 'user'
+);
+
